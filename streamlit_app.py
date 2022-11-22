50d05aba72a2d4865a20edbe7ae308eb92b90bf6
Vishnuprakash12 <118507976+Vishnuprakash12@users.noreply.github.com>
2022-11-22 15:00:09 +0530

    error exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+   streamlit.error("Please select a fruit to get information.")
+else:
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -37,6 +39,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display in dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+except URLerror as e:
+  streamlit.error()
 
 streamlit.stop()
 
